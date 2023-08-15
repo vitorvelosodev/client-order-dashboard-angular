a834c91f68266028145a04e402b154dbdfc734eb
Vitor Veloso <vitorvelosodev@gmail.com>
2023-08-15 11:37:23 -0300

    :lipstick: Create initial UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FilterInputComponent } from './filter-input/filter-input.component';
+import { ClientTableComponent } from './client-table/client-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterInputComponent,
+    ClientTableComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/filter-input/filter-input.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-input',
+  templateUrl: './filter-input.component.html',
+  styleUrls: ['./filter-input.component.css']
+})
+export class FilterInputComponent {
+
+}
